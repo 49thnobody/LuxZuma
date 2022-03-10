8f4e98b62e3da0aa5f297800e647d844666d2d1d
Elena <49th.nobody@gmail.com>
2022-03-10 17:57:59 +0300

    working on ball movement and collisions

--- VotansRunes/Assets/Scripts/BallController.cs
+++ VotansRunes/Assets/Scripts/BallController.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    private BoxCollider2D _collider;
+    private CircleCollider2D _collider;
     private SpriteRenderer _sprite;
     private Color _color;
     private BallState _state;
-    private float _speed = 5f;
+    private float _speed = 12f;
 
-    private Vector2 CurrentPath;
+    private Direction _currentDirection;
+
+    public delegate void BallCollision(BallController ballOnRoad, BallController movingBall);
+    public event BallCollision OnMovingBallCollision;
+    public event BallCollision OnRoadBallCollision;
 
     void Awake()
     {
         _sprite = GetComponentInChildren<SpriteRenderer>();
-        _collider = GetComponentInChildren<BoxCollider2D>();
+        _collider = GetComponentInChildren<CircleCollider2D>();
     }
 
     void Update()
@@ -26,20 +30,64 @@ public class BallController : MonoBehaviour
             pos.y += _speed * Time.deltaTime;
             transform.position = pos;
         }
-        if(_state == BallState.OnRoad)
+        if (_state == BallState.OnRoad)
         {
+            Vector3 pos = transform.position;
 
+            switch (_currentDirection)
+            {
+                case Direction.Left:
+                    pos.x -= _speed * Time.deltaTime;
+                    break;
+                case Direction.Rigth:
+                    pos.x += _speed * Time.deltaTime;
+                    break;
+                case Direction.Bottom:
+                    pos.y -= _speed * Time.deltaTime;
+                    break;
+                default:
+                    break;
+            }
+
+            transform.position = pos;
         }
     }
 
-    public void SetState(BallState state)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        _state = state;
+        if (_state == BallState.Moving) return;
+
+        var collisionBall = collision.gameObject.GetComponent<BallController>();
+        if (collisionBall == null) return;
+
+        if (collisionBall._state == BallState.OnRoad)
+            OnRoadBallCollision?.Invoke(this, collisionBall);
+        else if (collisionBall._state == BallState.Moving)
+            OnMovingBallCollision?.Invoke(this, collisionBall);
     }
 
     public void Set(Color color, Sprite sprite)
     {
         _color = color;
         _sprite.sprite = sprite;
+    }
+    public void SetState(BallState state)
+    {
+        _state = state;
+    }
+
+    public void SetDirection(Direction direction)
+    {
+        _currentDirection = direction;
+    }
+
+    public void SetSpeed(float speed)
+    {
+        _speed = speed;
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        transform.position = position;
     }
 }
--- VotansRunes/Assets/Scripts/RoadController.cs
+++ VotansRunes/Assets/Scripts/RoadController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RoadController : MonoBehaviour
@@ -11,6 +12,8 @@ public class RoadController : MonoBehaviour
 
     private float _baseSpeed = 3f;
     private float _speedOnStart = 15f;
+
+    private float _ballSize = 0.7f;
 
     private void Awake()
     {
@@ -25,12 +28,39 @@ public class RoadController : MonoBehaviour
         for (int i = 0; i < 25; i++)
         {
             _ballsOnRoad.AddLast(BallSpawner.instance.SpawnOnRoad());
-            
+            _ballsOnRoad.Last.Value.SetSpeed(1f);
+            _ballsOnRoad.Last.Value.OnMovingBallCollision += OnMovingBallCollision;
+            _ballsOnRoad.Last.Value.OnRoadBallCollision += OnRoadBallCollision; ;
         }
     }
 
-    private void Update()
+    private void OnRoadBallCollision(BallController ballOnRoad, BallController movingBall)
     {
-        
+        ReplaceBalls();
+    }
+
+    private void OnMovingBallCollision(BallController ballOnRoad, BallController movingBall)
+    {
+        var ballInList = _ballsOnRoad.Find(ballOnRoad);
+
+        if (ballInList != null) return;
+
+        // определить впихнуть до или после
+
+        // впихнуть
+
+        // передвинуть
+        ReplaceBalls();
+    }
+
+    private void ReplaceBalls()
+    {
+        // план: от последнего элемента к первому передвинуть шары друг к другу
+        var currentNode = _ballsOnRoad.Last;
+
+        for (int i = 0; i < _ballsOnRoad.Count - 1; i++)
+        {
+            
+        }
     }
 }
