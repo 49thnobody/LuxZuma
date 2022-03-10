130def3a2364563c452283c2f784d5ee360d8c89
Elena <49th.nobody@gmail.com>
2022-03-10 18:29:20 +0300

    refactoring

--- VotansRunes/Assets/Scripts/BallSpawner.cs
+++ VotansRunes/Assets/Scripts/BallSpawner.cs
@@ -17,22 +17,21 @@ public class BallSpawner : MonoBehaviour
     public Transform PlatformPorition;
     public BallController SpawnOnPlatform()
     {
-        var newBall = Instantiate(BallPrefab, PlatformPorition);
-        int color = Random.Range(0, BallSprites.Count - 1);
-        newBall.Set((Color)color, BallSprites[color]);
-        newBall.SetState(BallState.Active);
-
-        return newBall;
+        return SpawnOnPlace(PlatformPorition, BallState.Active);
     }
 
     public Transform RoadSpawnPosition;
     public BallController SpawnOnRoad()
     {
-        var newBall = Instantiate(BallPrefab, RoadSpawnPosition);
+        return SpawnOnPlace(RoadSpawnPosition, BallState.OnRoad);
+    }
+
+    private BallController SpawnOnPlace(Transform parent, BallState ballState)
+    {
+        var newBall = Instantiate(BallPrefab, parent);
         int color = Random.Range(0, BallSprites.Count - 1);
         newBall.Set((Color)color, BallSprites[color]);
-        newBall.SetState(BallState.OnRoad);
-
+        newBall.SetState(ballState);
         return newBall;
     }
 }
